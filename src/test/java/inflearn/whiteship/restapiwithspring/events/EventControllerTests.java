ac5cce404f297df6b5b7b2e61196d6d1c32bad6f
BeomSeogKim <kbs4520@daum.net>
2022-11-14 20:47:44 +0900

    Event Controller 생성

--- src/test/java/inflearn/whiteship/restapiwithspring/events/EventControllerTests.java
+++ src/test/java/inflearn/whiteship/restapiwithspring/events/EventControllerTests.java
@@ -1,5 +1,6 @@
 package inflearn.whiteship.restapiwithspring.events;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +10,11 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
@@ -19,13 +24,34 @@ public class EventControllerTests {
     @Autowired
     MockMvc mockMvc;
 
+    @Autowired
+    ObjectMapper objectMapper;
+
     @Test
     public void createEvent() throws Exception {
+
+        // Event 생성
+        Event event = Event.builder()
+                .name("Spring")
+                .description("REST API Development with Spring")
+                .beginEnrollmentDateTime(LocalDateTime.of(2022, 11, 14, 20, 32))
+                .closeEnrollmentDateTime(LocalDateTime.of(2022, 11, 15, 20, 32))
+                .beginEventDateTime(LocalDateTime.of(2022, 11, 16, 20, 32))
+                .endEventDateTime(LocalDateTime.of(2022, 11, 17, 20, 32))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("강남역 D2 스타트업 팩토리")
+                .build();
+
+
         mockMvc.perform(post("/api/events/")
                         .contentType(MediaType.APPLICATION_JSON_UTF8)
                         .accept(MediaTypes.HAL_JSON)
-                )
-                .andExpect(status().isCreated());
+                        .content(objectMapper.writeValueAsString(event)))
+                .andDo(print())  // 요청받은 정보를 테스트 상에서 확인 할 수 있음
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("id").exists());    // id가 나오는지 확인
     }
 
 }
